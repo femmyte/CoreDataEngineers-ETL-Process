df97468b704a36bf81f6267cfdda6cc3feb51875
femmyte <sanyaoluadefemi@gmail.com>
2024-09-05 10:43:02 +0100

    make update

--- scripts/sql_scripts/copy_file_to_database.sh
+++ scripts/sql_scripts/copy_file_to_database.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Variables
-DB_NAME="newposey"
-DB_USER="posgress"
+DB_NAME="posey"
+DB_USER="postgres"
 DB_HOST="localhost"
 DB_PORT="5432"
 CSV_DIR="../../raw/parch_posey"
@@ -10,7 +10,7 @@ CSV_DIR="../../raw/parch_posey"
 # Iterate over each CSV file in the directory
 for file in "$CSV_DIR"/*.csv
 do
-  # Extract table name from the CSV file name (assuming the table name is the same as the file name without extension)
+  # Extract table name from the CSV file name 
   table_name=$(basename "$file" .csv)
 
   echo "Processing $file for table $table_name"
@@ -18,7 +18,7 @@ do
   # Read the first line (header) of the CSV file to get column names
   header=$(head -n 1 "$file")
   
-  # Generate the table schema (assumes all columns are TEXT)
+  # Generate the table schema
   schema=""
   IFS=',' read -ra columns <<< "$header"
   for col in "${columns[@]}"; do
@@ -28,14 +28,21 @@ do
   schema=${schema%,}  # Remove trailing comma
 
   # Create the table with the generated schema
+  echo 'the schema ' $schema
   psql -U "$DB_USER" -d "$DB_NAME" -h "$DB_HOST" -p "$DB_PORT" -c "
     CREATE TABLE IF NOT EXISTS \"$table_name\" ($schema);
   "
 
   # Import the CSV file into the PostgreSQL table
-  psql -U "$DB_USER" -d "$DB_NAME" -h "$DB_HOST" -p "$DB_PORT" -c "
-    COPY \"$table_name\" FROM '$file' WITH CSV HEADER;
-  "
+  # psql -U "$DB_USER" -d "$DB_NAME" -h "$DB_HOST" -p "$DB_PORT" -c "
+  #   LOAD DATA LOCAL INFILE '$table_name' INTO TABLE "$table_name"
+  #   FIELDS TERMINATED BY ','
+  #   LINES TERMINATED BY '\n'
+  #   ENCLOSED BY '\"'
+  #   ESCAPE '\"'
+  #   IGNORE 1 LINES;
+  #   # COPY \"$table_name\" FROM '$file' WITH CSV HEADER;
+  # "
 
   echo "$file imported successfully into table $table_name"
 done
--- scripts/sql_scripts/transform.sql
+++ scripts/sql_scripts/transform.sql
@@ -10,14 +10,14 @@ SELECT *
 FROM orders
 WHERE standard_qty = 0 AND (gloss_qty > 1000 or poster_qty > 1000);
 
-
 -- this is not correct yet
 /* Find all the company names that start with a 'C' or 'W', and where the primary contact contains 'ana' or 'Ana', but does not contain 'eana'. */
 
 SELECT *
 FROM accounts
-WHERE name LIKE 'C%' OR name LIKE 'W%' AND (primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%' AND primary_poc NOT LIKE '%eana%' );
-
+WHERE (name LIKE 'C%' OR name LIKE 'W%') 
+	AND ( (primary_poc LIKE '%ana%' OR primary_poc LIKE '%Ana%') 
+	AND primary_poc NOT LIKE '%eana%');
 /* Provide a table that shows the region for each sales rep along with their associated accounts. Your final table should include three columns: the region name, the sales rep name, and the account name. Sort the accounts alphabetically (A-Z) by account name. */
 
 select region.name as region_name, sales_reps.name as sales_rep_name, accounts.name as account_name
